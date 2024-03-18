520d45f7f79786c5a00aca5443e359fad4989abd
ZiarayZ <jakesmith@binaryrage.com>
2024-03-18 11:24:49 +0000

    move if into loop

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
+import asyncio
 from src.config.config import fetch_config
 
+
+# Try to declare all your globals at once to facilitate compilation later.
+COUNT_DOWN = 20
+
+# Do init here
+# Load any assets right now to avoid lag at runtime or network errors.
 config = fetch_config()
 
-if __name__ == "__main__" and config != None:
-    import asyncio
 
-    # Try to declare all your globals at once to facilitate compilation later.
-    COUNT_DOWN = 20
+async def main():
+    global COUNT_DOWN
 
-    # Do init here
-    # Load any assets right now to avoid lag at runtime or network errors.
+    while True:
 
-    async def main():
-        global COUNT_DOWN
-
-        while True:
-
-            # Do your rendering here, note that it's NOT an infinite loop,
-            # and it is fired only when VSYNC occurs
-            # Usually 1/60 or more times per seconds on desktop
-            # could be less on some mobile devices
-
+        # Do your rendering here, note that it's NOT an infinite loop,
+        # and it is fired only when VSYNC occurs
+        # Usually 1/60 or more times per seconds on desktop
+        # could be less on some mobile devices
+        if __name__ == "__main__" and config != None:
             print(
                 f"""
 
@@ -28,18 +27,19 @@ if __name__ == "__main__" and config != None:
 
     """
             )
-            # pygame.display.update() should go right next line
+        # pygame.display.update() should go right next line
 
-            await asyncio.sleep(0)  # Very important, and keep it 0
+        await asyncio.sleep(0)  # Very important, and keep it 0
 
-            if not COUNT_DOWN:
-                return
+        if not COUNT_DOWN:
+            return
 
-            COUNT_DOWN = COUNT_DOWN - 1
+        COUNT_DOWN = COUNT_DOWN - 1
 
-    # This is the program entry point:
-    asyncio.run(main())
 
-    # Do not add anything from here, especially sys.exit/pygame.quit
-    # asyncio.run is non-blocking on pygame-wasm and code would be executed
-    # right before program start main()
+# This is the program entry point:
+asyncio.run(main())
+
+# Do not add anything from here, especially sys.exit/pygame.quit
+# asyncio.run is non-blocking on pygame-wasm and code would be executed
+# right before program start main()
